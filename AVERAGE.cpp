285e843ffcd2ba01e740def6ae1fbd5bb12b0479
vegebirrrrd <1458127988@qq.com>
2024-09-22 14:24:38 +0800

    Add files via upload

---
+++ AVERAGE.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cout << "enter first number:";
+	cin >> a;
+	cout << "enter second number:";
+	cin >> b;
+	cout << "enter third number:";
+	cin >> c;
+
+	d = (a + b + c) / 3;
+
+	cout << "the average of " << a << " and " << b << " and " << c << " is " << d << endl;
+
+	return 114514;
+
+
+
+
+
+
+}
